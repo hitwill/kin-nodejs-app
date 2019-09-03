2e7a7b1911a43bfa4e0fa23efd651f9dff5e08f4
Chase Barker <chasebarker@Chases-iMac.attlocal.net>
2019-09-03 15:16:07 -0400

    kincontroller formatting

--- app/routes/KinController.js
+++ app/routes/KinController.js
@@ -16,74 +16,71 @@ const account = client.createKinAccount({
         });
 
 app.post('/create', function(req, res) {
-
-    console.log(req.body.address);
-
-    client.getMinimumFee()
-        .then(minFee => {
-            account.buildCreateAccount({
-                address: req.body.address,
-                startingBalance: 100,
-                fee: minFee,
-                memoText: 'kinwallet'
-            }).then(createAccountBuilder => {
-                return account.submitTransaction(createAccountBuilder)
-            }).then(transactionId => {
-                console.log(transactionId);
-                res.json({transactionId: transactionId});
-            }).catch( err => {
-                console.log(err);
-            });
-        }).catch(err => {
-            console.log(err);
-        });
+  client.getMinimumFee()
+      .then(minFee => {
+          account.buildCreateAccount({
+              address: req.body.address,
+              startingBalance: 100,
+              fee: minFee,
+              memoText: 'kinwallet'
+          }).then(createAccountBuilder => {
+              return account.submitTransaction(createAccountBuilder)
+          }).then(transactionId => {
+              console.log(transactionId);
+              res.json({transactionId: transactionId});
+          }).catch( err => {
+              console.log(err);
+          });
+      }).catch(err => {
+          console.log(err);
+      });
 });
 
 app.get('/account', function(req, res) {
-
-  console.log(req.body.address);
-
-    client.getAccountData(req.body.address)
-        .then(accountData => {
-            console.log(accountData);
-            res.json({accountData: accountData});
-        })
+  client.getAccountData(req.body.address)
+    .then(accountData => {
+        console.log(accountData);
+        res.json({accountData: accountData});
+    })
+    .catch(err => {
+      
+    })
 });
 
 app.post('/pay', function(req, res) {
-    client.getMinimumFee()
-        .then(minFee => {
-            account.buildSendKin({
-                address: req.body.address,
-                amount: 100,
-                fee: minFee,
-                memoText: 'tx memo'
-            }).then(builder => {
-                return account.submitTransaction(builder)
-            }).then(transactionId => {
-                console.log(transactionId);
-                res.json({transactionId: transactionId});
-            });
+  client.getMinimumFee()
+    .then(minFee => {
+        account.buildSendKin({
+            address: req.body.address,
+            amount: 100,
+            fee: minFee,
+            memoText: 'tx memo'
+        }).then(builder => {
+            return account.submitTransaction(builder)
+        }).then(transactionId => {
+            console.log(transactionId);
+            res.json({transactionId: transactionId});
         });
+    });
 });
 
 app.get('/balance', function(req, res) {
-    client.getAccountBalance(req.body.address)
-        .then(balance => {
-            console.log(balance);
-            res.json({balance: balance})
-        });
+  client.getAccountBalance(req.body.address)
+    .then(balance => {
+        console.log(balance);
+        res.json({balance: balance})
+    });
 });
 
 app.get('/transaction', function(req, res) {
-    client.getTransactionData(req.body.transactionId)
-        .then(transactionData => {
-            console.log(transactionData)
-            res.json({transactionData: transactionData});
-        });
+  client.getTransactionData(req.body.transactionId)
+    .then(transactionData => {
+        console.log(transactionData)
+        res.json({transactionData: transactionData});
+    });
 });
 
-app.post('/whitelist', function(req, res) {
+app.get('/whitelist', function(req, res) {
 
     let whitelistTransaction = account.whitelistTransaction({ envelope: req.body.envelope, networkId : req.body.network_id});
 
