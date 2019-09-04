a856ec31e261b4031320373ea582e847f0f99561
Chase Barker <chasebarker@Chases-iMac.attlocal.net>
2019-09-04 15:08:29 -0400

    updated get all user and get user by id to async/await

--- app/routes/UserController.js
+++ app/routes/UserController.js
@@ -35,36 +35,35 @@ router.post('/', function(req, res, next) {
 });
 
 // Returns all users from the database
-router.get('/', function (req, res) {
-    User
+router.get('/', async function (req, res) {
+  try{
+    const users = await User
       .find()
       .select("-password")
       .exec()
-      .then(users => {
-        res.status(200).json(users);
-      })
-      .catch(err => {
-        res.status(500).json({error: err});
-      });
+
+      res.status(200).json(users);
+    } catch(err){
+      console.log(err);
+      res.status(500).json({error: err});
+    }
 });
 
 // Returns a single user from the database
-router.get('/:id', function (req, res) {
-    User
+router.get('/:id', async function (req, res) {
+  try{
+    const user = await User
       .findById(req.params.id)
       .select("-password")
       .exec()
-      .then(user => {
-        res.status(200).json(user);
-      })
-      .catch(err => {
-        res.status(500).json({error: err});
-      });
+    res.status(200).json(user);
+    } catch (err){
+      res.status(500).json({error: err});
+    }
 });
 
 // Deletes a single user from the database
 router.delete('/:id', function (req, res) {
-
     User.findById(req.params.id)
     .exec()
     .then(user =>{
@@ -77,16 +76,6 @@ router.delete('/:id', function (req, res) {
     .catch(err => {
       res.status(500).json({error: err});
     });
-
-    // User
-    //   .findByIdAndRemove(req.params.id)
-    //   .exec()
-    //   .then(user => {
-    //     res.status(200).json(user + ' deleted');
-    //   })
-    //   .catch(err => {
-    //     res.status(500).json({error: err});
-    //   });
 });
 
 // Updates a single user in the database
